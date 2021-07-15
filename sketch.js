42a1852abf7247fa23b4dd75a684314ef403bf06
AafreenKhan040896 <77886222+AafreenKhan040896@users.noreply.github.com>
2021-07-16 00:53:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,73 @@
+var bath1, bath2;
+var eat1, eat2;
+var gym1, gym2, gym11, gym12;
+var move, modrink1, drink2;
+var eat1, ve1;
+var iss, brush, sleep;
+var edges;
+
+
+function preload() {
+
+  bg = loadImage("iss.png");
+  sleep = loadAnimation("sleep.png");
+  brush = loadAnimation("brush.png");
+  gym = loadAnimation("gym1.png", "gym2.png", "gym11.png", "gym12.png");
+  eat = loadAnimation("eat1.png", "eat2.png");
+  drink = loadAnimation("drink1.png", "drink2.png");
+  bath = loadAnimation("bath1.png", "bath2.png");
+  move = loadAnimation("move.png", "move1.png","move.png","move1.png");
+}
+
+function setup() {
+  createCanvas(800, 400);
+
+  bgg = createSprite(400, 200);
+  bgg.addImage(bg);
+  bgg.scale = 0.2;
+
+  astronaut = createSprite(300, 200);
+  astronaut.addAnimation("sleeping", sleep);
+  astronaut.scale = 0.1;
+
+  edges = createEdgeSprites();
+
+}
+
+function draw() {
+  astronaut.bounceOff(edges);
+
+  if (keyDown("UP_ARROW")) {
+    astronaut.addAnimation("brushing", brush);
+    astronaut.changeAnimation("brushing");
+    astronaut.y = 200;
+    astronaut.velocityX = 0;
+    astronaut.velocityY = 0;
+  }
+  if (keyDown("DOWN_ARROW")) {
+    astronaut.addAnimation("gym", gym);
+    astronaut.changeAnimation("gym");
+  }
+  if (keyDown("LEFT_ARROW")) {
+    astronaut.addAnimation("bath", bath);
+    astronaut.changeAnimation("bath");
+  }
+  if (keyDown("RIGHT_ARROW")) {
+    astronaut.addAnimation("eat", eat);
+    astronaut.changeAnimation("eat");
+  }
+  if (keyDown("d")) {
+    astronaut.addAnimation("drink", drink);
+    astronaut.changeAnimation("drink");
+  }
+  if (keyDown("m")) {
+    astronaut.addAnimation("moving", move);
+    astronaut.changeAnimation("moving");
+    astronaut.velocityX = 1;
+    astronaut.velocityY = 1;
+
+
+  }
+
+  drawSprites();
+}
